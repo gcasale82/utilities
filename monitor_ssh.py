32f612ac308604c2633cf62bf1ad04d24d596193
gcasale82 <81979394+gcasale82@users.noreply.github.com>
2021-05-10 10:42:07 +0200

    Add files via upload
    added syslog,log rotation ,daemon,

--- monitor_ssh.py
+++ monitor_ssh.py
@@ -2,52 +2,100 @@
 import paramiko
 from apscheduler.schedulers.blocking import BlockingScheduler
 from pysnmp.hlapi import *
+import argparse
+import getpass
+import logging
+import logging.handlers
+import os
+import daemon
+import datetime
 
 
-def ssh_test() :
-    #host = "test.rebex.net"
-    host = "10.1.1.1"
-    port = 22
-    username = "demo"
-    password = "password"
-    ssh = paramiko.SSHClient()
-    connection_error = False
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    try :
-        ssh.connect(host, port, username, password,timeout=5)
-        ssh.close()
-    except  :
-        connection_error = True
-    return connection_error
-
-def test_job():
-    if ssh_test() :
-        print("SSH connection failed")
-        send_trap()
-    else : print("SSH connection is alive")
+class test_ssh():
+    def __init__(self,host,user,password,interval,unit):
+        self.host=host
+        self.user=user
+        self.password = password
+        self.interval=interval
+        self.unit = unit
+        self.scheduler = BlockingScheduler()
+        self.connection_error = False
 
 
-def send_trap():
-    iterator = sendNotification(
-        SnmpEngine(),
-        CommunityData('community_test', mpModel=0),
-        UdpTransportTarget(('74.1.1.1', 162)),
-        ContextData(),
-        'trap',
-        NotificationType(
-            ObjectIdentity('1.3.6.1.6.3.1.1.5.2')
-        ).addVarBinds(
-            ('1.3.6.1.6.3.1.1.4.3.0', '1.3.6.1.4.1.20408.4.1.1.2'),
-            ('1.3.6.1.2.1.1.1.0', OctetString('Smos backup connection failed'))
-        ).loadMibs(
-            'SNMPv2-MIB'
+    def ssh_test(self):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            ssh.connect(self.host, 22 , self.user, self.password , timeout=5)
+            ssh.close()
+        except:
+            self.connection_error = True
+
+    def write_log(self):
+        file_path = r'/var/log/monitor_ssh.log'
+        logging.basicConfig(filename= file_path ,
+                            #format='%(asctime)s %(message)s',
+                            filemode='w')
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+        if self.connection_error : logger.error(f"{datetime.datetime.now()} - SSH connection to {self.host} was lost , SNMP trap has been sent to Solarwinds ")
+        else : logger.info(f"{datetime.datetime.now()} - SSH connection to {self.host} is alive ")
+        handler = logging.handlers.RotatingFileHandler(file_path , maxBytes=200000,
+                                      backupCount=5)
+        logger.addHandler(handler)
+
+    def test_job(self):
+        self.ssh_test()
+        if self.connection_error :
+            self.send_trap()
+            self.write_log()
+        else:
+            self.write_log()
+
+
+    def send_trap(self):
+        iterator = sendNotification(
+            SnmpEngine(),
+            CommunityData('community_test', mpModel=0),
+            UdpTransportTarget(('74.1.1.1', 162)),
+            ContextData(),
+            'trap',
+            NotificationType(
+                ObjectIdentity('1.3.6.1.6.3.1.1.5.2')
+            ).addVarBinds(
+                ('1.3.6.1.6.3.1.1.4.3.0', '1.3.6.1.4.1.20408.4.1.1.2'),
+                ('1.3.6.1.2.1.1.1.0', OctetString('Smos backup connection failed'))
+            ).loadMibs(
+                'SNMPv2-MIB'
+            )
         )
-    )
-    next(iterator)
+        next(iterator)
+    def run_job(self):
+        if self.unit == "seconds" :
+            self.scheduler.add_job(self.test_job, 'interval', seconds = self.interval)
+        elif self.unit == "minutes" :
+            self.scheduler.add_job(self.test_job, 'interval', minutes=self.interval)
+        elif self.unit == "hours" :
+            self.scheduler.add_job(self.test_job, 'interval', hours=self.interval)
+        self.scheduler.start()
 
 
-scheduler = BlockingScheduler()
-#scheduler.add_job(some_job, 'interval', hours=1)
-scheduler.add_job(test_job, 'interval', seconds=10)
-scheduler.start()
+def main() :
+    my_parser = argparse.ArgumentParser(prog='monitor_ssh',
+                                        usage='%(prog)s host_ip_address user interval seconds/minutes/hours',
+                                        description='Monitor ssh connection , log report , snmp trap generation ')
 
+    my_parser.add_argument('--host', action='store', type=str, required=True, help="host ip address to be monitored")
+    my_parser.add_argument('--user', action='store', type=str, required=True, help="user for the ssh connection")
+    my_parser.add_argument('--interval', action='store', type=int, required=True, help="interval of job scheduler")
+    my_parser.add_argument('--unit', action='store', choices=['seconds', 'minutes', 'hours'])
+    args=my_parser.parse_args()
+    password = getpass.getpass()
+    with daemon.DaemonContext():
+        run_ssh = test_ssh(args.host,args.user,password, args.interval,args.unit)
+        run_ssh.run_job()
+
+
+
+
+main()
